b3b7eefb0630fa7a2596f895b15b6af2f5518479
Peilonrayz <joe@thewallisfamily.co.uk>
2013-10-07 17:20:22 +0100

    Sam and Jow

--- Server.py
+++ Server.py
@@ -3,51 +3,21 @@ import settings as s
     
 urls = (
     '/', 'Index',
-    '/about', 'About',
-    '/blog', 'Blog',
-    '/projects', 'Projects',
-    '/python', 'Python'
+    '/sam', 'Sam',
+    '/joe', 'Joe',
 )
-
-def Make(func, name, funcParse = [""]):
-    if len(funcParse) and funcParse != [""]:
-        return str(s.render.header(name))+str(func(funcParse))+str(s.render.footer())
-    return str(s.render.header(name))+str(func())+str(s.render.footer())
 
 class Index:
     def GET(self):
-        return Make(s.render.index, "Peilonrayz")
+        return s.render.index()
 
-class About:
+class Joe:
     def GET(self):
-        return Make(s.render.about, "About")
+        return s.render.joe()
 
-class Blog:
+class Sam:
     def GET(self):
-        i = web.input(b="")
-        if i['b'] != "":
-            b = s.Blogs._lookup(i['b'])[1]
-            if b != None:
-                return s.Format([Make(s.Blogs._template(i['b']), s.BlogsList[int(i['b'])])])
-        return Make(s.render.blog, "Blog", s.BlogsList)
-
-class Projects:
-    def GET(self):
-        i = web.input(p="")
-        if i['p'] != "":
-            p = s.Projects._lookup(i['p'])[1]
-            if p != None:
-                return s.Format([Make(s.Projects._template(i['p']), s.ProjectsList[int(i['p'])])])
-        return Make(s.render.projects, "Projects", s.ProjectsList)
-
-class Python:
-    def GET(self):
-        i = web.input(p="")
-        if i['p'] != "":
-            p = s.Python._lookup(i['p'])[1]
-            if p != None:
-                return s.Format([Make(s.Python._template(i['p']), s.PythonList[int(i['p'])])])
-        return Make(s.render.python, "Python", s.PythonList)
+        return s.render.sam()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
