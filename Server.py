03dc94f6f77126e2f710c7f180a0c8a75fd586f4
Peilonrayz <joe@thewallisfamily.co.uk>
2013-10-07 17:36:09 +0100

    Updated server to have Sam and Joe

--- Server.py
+++ Server.py
@@ -7,17 +7,22 @@ urls = (
     '/joe', 'Joe',
 )
 
+def Make(func, name, funcParse = [""]):
+    if len(funcParse) and funcParse != [""]:
+        return str(s.render.header(name))+str(func(funcParse))+str(s.render.footer())
+    return str(s.render.header(name))+str(func())+str(s.render.footer())
+
 class Index:
     def GET(self):
-        return s.render.index()
+        return Make(s.render.index, "Quarkboard")
 
 class Joe:
     def GET(self):
-        return s.render.joe()
+        return Make(s.render.joe, "Peilonrayz")
 
 class Sam:
     def GET(self):
-        return s.render.sam()
+        return Make(s.render.sam, "Mr. Cardboardman")
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
