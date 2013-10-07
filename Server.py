a611952738e6a94102cc6b92d14c6c60434e0bff
Peilonrayz <joe@thewallisfamily.co.uk>
2013-10-07 17:20:37 +0100

    Revert "Sam and Jow"
    This reverts commit b3b7eefb0630fa7a2596f895b15b6af2f5518479.

--- Server.py
+++ Server.py
@@ -3,21 +3,51 @@ import settings as s
     
 urls = (
     '/', 'Index',
-    '/sam', 'Sam',
-    '/joe', 'Joe',
+    '/about', 'About',
+    '/blog', 'Blog',
+    '/projects', 'Projects',
+    '/python', 'Python'
 )
+
+def Make(func, name, funcParse = [""]):
+    if len(funcParse) and funcParse != [""]:
+        return str(s.render.header(name))+str(func(funcParse))+str(s.render.footer())
+    return str(s.render.header(name))+str(func())+str(s.render.footer())
 
 class Index:
     def GET(self):
-        return s.render.index()
+        return Make(s.render.index, "Peilonrayz")
 
-class Joe:
+class About:
     def GET(self):
-        return s.render.joe()
+        return Make(s.render.about, "About")
 
-class Sam:
+class Blog:
     def GET(self):
-        return s.render.sam()
+        i = web.input(b="")
+        if i['b'] != "":
+            b = s.Blogs._lookup(i['b'])[1]
+            if b != None:
+                return s.Format([Make(s.Blogs._template(i['b']), s.BlogsList[int(i['b'])])])
+        return Make(s.render.blog, "Blog", s.BlogsList)
+
+class Projects:
+    def GET(self):
+        i = web.input(p="")
+        if i['p'] != "":
+            p = s.Projects._lookup(i['p'])[1]
+            if p != None:
+                return s.Format([Make(s.Projects._template(i['p']), s.ProjectsList[int(i['p'])])])
+        return Make(s.render.projects, "Projects", s.ProjectsList)
+
+class Python:
+    def GET(self):
+        i = web.input(p="")
+        if i['p'] != "":
+            p = s.Python._lookup(i['p'])[1]
+            if p != None:
+                return s.Format([Make(s.Python._template(i['p']), s.PythonList[int(i['p'])])])
+        return Make(s.render.python, "Python", s.PythonList)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
